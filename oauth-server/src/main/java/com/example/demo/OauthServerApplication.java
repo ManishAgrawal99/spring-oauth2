919eb93cfdb7688b1bd0d17849b2fa7211f40130
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-04-30 16:43:19 +0530

    In-Memory OAuth Server

--- oauth-server/src/main/java/com/example/demo/OauthServerApplication.java
+++ oauth-server/src/main/java/com/example/demo/OauthServerApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class OauthServerApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,16 @@ public class OauthServerApplication {
 	}
 
 }
+
+/*
+ * To get Access Token
+ * 
+ * POST REQUEST
+ * localhost:8282/oauth/token?grant_type=password&username=Manish&password=ManishPass
+ * 
+ * 
+ * To Check Token
+ * localhost:8282/oauth/check_token?token=bc7cb519-081b-46d0-88fa-5d7800d863f3
+ * 
+ * **/
+ 
---
+++ oauth-server/src/main/java/com/example/demo/config/UserConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+	PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	
+	@Override
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication().withUser("Manish")
+									 .password(passwordEncoder.encode("ManishPass"))
+									 .roles("USER", "ADMIN", "MANAGER")
+									 .authorities("CAN_READ", "CAN_WRITE", "CAN_DELETE")
+									 .and()
+									 .withUser("Manish2")
+									 .password(passwordEncoder.encode("ManishPass2"))
+									 .roles("USER")
+									 .authorities("CAN_READ", "CAN_WRITE");
+	}
+	
+	
+}
